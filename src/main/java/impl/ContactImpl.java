296785d44477b916e3f328d53e42df9a683ff3e1
Pedro Ferreira <pbf@frameweb.net>
2017-03-18 23:01:21 +0000

    Pass getName

--- src/main/java/impl/ContactImpl.java
+++ src/main/java/impl/ContactImpl.java
@@ -46,7 +46,7 @@ public class ContactImpl implements Contact {
      */
     @Override
     public String getName() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.name;
     }
 
     /**
