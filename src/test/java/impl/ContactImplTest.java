f611f389ab5e6d88152a9a95210e777e25d4c1fb
Pedro Ferreira <pbf@frameweb.net>
2017-03-18 21:48:44 +0000

    Create ContactImplTest skeleton

---
+++ src/test/java/impl/ContactImplTest.java
@@ -0,0 +1,35 @@
+package impl;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * ContactImplTest
+ *
+ * @author Pedro Ferreira <pbf@frameweb.net>
+ */
+public class ContactImplTest {
+    
+    public ContactImplTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+}
