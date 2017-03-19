cb18b0a6904ae6e537c20aa62a76cf8c9fbcfd5f
Pedro Ferreira <pbf@frameweb.net>
2017-03-19 17:53:13 +0000

    Fail testAddNewContact

--- src/test/java/impl/ContactManagerImplTest.java
+++ src/test/java/impl/ContactManagerImplTest.java
@@ -1,11 +1,13 @@
 package impl;
 
+import java.util.Set;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import spec.Contact;
 import spec.ContactManager;
 
 /**
@@ -41,6 +43,20 @@ public class ContactManagerImplTest {
         } catch (Exception e) {
             fail(e.getMessage());
         }
+    }
+
+    @Test
+    public void testAddNewContact() {
+        ContactManager manager = new ContactManagerImpl();
+        String name = "Pedro";
+        String notes = "Some notes";
+        int id = manager.addNewContact(name, notes);
+        Set<Contact> contacts = manager.getContacts(name);
+        assertEquals(1, contacts.size());
+        Contact contact = contacts.iterator().next();
+        assertEquals(id, contact.getId());
+        assertEquals(name, contact.getName());
+        assertEquals(notes, contact.getNotes());
     }
 
 }
